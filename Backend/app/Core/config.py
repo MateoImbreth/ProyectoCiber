ccfc10450d84cdc324e619826dd42bf74cdf7008
Mateo Imbreth <imbrethmateo10@gmail.com>
2026-02-13 16:28:22 -0500

    Modificar el tiempo de duración del token

--- Backend/app/Core/config.py
+++ Backend/app/Core/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Settings:
     SECRET_KEY : str = os.environ.get("SECRET_KEY")
-    TOKEN_SECONDS_EXP: int = 20
+    TOKEN_SECONDS_EXP: int = 2000
 
     DB_USER: str = os.environ.get("DB_USER")
     DB_PASSWORD: str = os.environ.get("DB_PASSWORD")
